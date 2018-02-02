6eff3abb127bd12659188e79b0bb948625660afd
Paweł Ambrożej <Amper@pawel-macbook-air.local>
2018-02-02 17:29:00 +0000

    Get rid of storyboard

--- ListMaster/AppDelegate.swift
+++ ListMaster/AppDelegate.swift
@@ -16,8 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
         UIApplication.shared.statusBarStyle = .lightContent
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        window?.rootViewController = MainVC()
         return true
     }
 
